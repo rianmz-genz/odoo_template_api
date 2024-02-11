f5f120853dcdc7f5bceb87b5c88a6edcc75de136
rianmz-genz <adrianji1945@gmail.com>
2024-02-11 08:46:21 +0700

    Iya

--- odoo_template_api/controllers/__init__.py
+++ odoo_template_api/controllers/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from . import base_controller
-from . import auth_controller_impl
-from . import project_controller_impl
+
 from . import auth_controller
+from . import auth_controller_impl
+
 from . import project_controller
+from . import project_controller_impl
